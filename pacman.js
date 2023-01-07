fa27efa034067e502b2ded04dc13881e17cea495
Gregor Zavodnik <gregor264@gmx.de>
2023-01-07 18:39:51 +0100

    Writing some Properties / Functions

---
+++ pacman.js
@@ -0,0 +1,44 @@
+class Pacman {
+    constructor (x, y, width, height) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+    }
+
+    moveProcess() {
+
+    }
+
+    eat() {
+
+    }
+
+    moveBackwards() {
+
+    }
+
+    moveForwards() {
+
+    }
+
+    checkCollision() {
+
+    }
+
+    checkGhostCollision() {
+
+    }
+
+    changeDirectionIfPossible() {
+
+    }
+
+    changeAnimation() {
+
+    }
+
+    draw() {
+        
+    }
+}
