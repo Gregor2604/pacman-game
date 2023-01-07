7ab8ff8971d7a7a372311807e86230010ad928ae
Gregor Zavodnik <gregor264@gmx.de>
2023-01-07 15:10:19 +0100

    The basic programm that will draw the walls

--- game.js
+++ game.js
@@ -9,7 +9,17 @@ let createRect = (x, y, width, height, color) =>{
 }
 
 
-//  This will be the Map!
+// Game variables
+let fps = 30;
+let pacman;
+let oneBlockSize = 20;
+let score = 0;
+let ghosts = [];
+let wallSpaceWidth = oneBlockSize / 1.6;
+let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
+let wallInnerColor = "black";
+
+// This will be the Map!
 // 0 = No Wall, you can't move there
 // 1 = Wall
 // 2 = No Wall, but at this space you can move
@@ -38,3 +48,38 @@ let map = [
     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
+
+let gameLoop = () => {
+    update();
+    draw();
+};
+
+let update = () => {
+    
+};
+
+let draw = () => {
+    drawWalls();
+};
+
+let gameInterval = setInterval(gameLoop, 1000 / fps);
+
+
+// This statements will draw the walls
+// If a Wall exists the i and j will the equal 1
+// Otherwise i and j will create one
+let  drawWalls = () =>{
+    for(let i = 0; i < map.length; i++){
+        for(let j = 0; j < map[0].length; j++){
+            if(map[i][j] == 1) {//The 1 does mean that there is a Wall and you can go throw there!
+                createRect(
+                    j * oneBlockSize, 
+                    i * oneBlockSize, 
+                    oneBlockSize, 
+                    oneBlockSize,
+                    "#342DCA"
+                );
+            }
+        }
+    }
+};
