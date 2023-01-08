1e7d83d67007a735d9ffe4d899f3a6791fcbbd59
Gregor Zavodnik <gregor264@gmx.de>
2023-01-08 11:49:08 +0100

    The moveForwards function is done

--- pacman.js
+++ pacman.js
@@ -4,10 +4,16 @@ class Pacman {
         this.y = y
         this.width = width
         this.height = height
+        this.speed = this.speed
+        this.direction = DIRECTION_RIGHT //This will be the Start Direction
     }
 
     moveProcess() {
-
+        this. changeDirectionIfPossible();
+        this.moveForwards()
+        if (this.checkCollision()) {
+            this.moveBackwards();
+        }
     }
 
     eat() {
@@ -19,7 +25,20 @@ class Pacman {
     }
 
     moveForwards() {
-
+        switch(this.direction) {
+            case DIRECTION_RIGHT:
+                this.x += this.speed // I write "+=" because the start direction is right
+                break;
+            case DIRECTION_UP:
+                this.y -= this.speed
+                break;
+            case DIRECTION_LEFT:
+                this.x -= this.speed
+                break;
+            case DIRECTION_BOTTOM:
+                this.y += this.speed
+                break;
+        }
     }
 
     checkCollision() {
